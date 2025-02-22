b998f0dff4b77ed9ba112ff28daac0c6f7a4cb87
duccini <guilhermeduccini@gmail.com>
2025-02-22 17:45:42 -0300

    create a struct of deck

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>
 }
+
+fn main() {
+    // List of 'suits' - 'hearts', 'spades'
+    let suits = ["Hearts", "Spades", "Diamonds"];
+
+    // List of 'values' - 'ace', 'two'
+    let values = ["Ace", "Two", "Three"];
+
+    let mut cards = vec![];
+
+    // Double nested for loop
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+
+    println!("Heres your deck: {:#?}", deck);
+}
+
