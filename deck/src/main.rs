737523110749d66f5c4770043deda1598e56432d
duccini <guilhermeduccini@gmail.com>
2025-02-22 22:38:47 -0300

    create split method

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -29,16 +29,28 @@ impl Deck {
     return deck;
   }
 
-  fn shuffle(&self) {
+  fn shuffle(&mut self) {
+    let mut rng = rng();
+    self.cards.shuffle(&mut rng); // recursive function?
+  }
 
+  fn deal(&mut self, num_cards: usize) -> Vec<String> {
+    // split_off: creates a new vector with the elements extracted from the elments
+    // at index given to the end of the array
+    self.cards.split_off(self.cards.len() - num_cards)
   }
 }
 
 fn main() {
-  let deck = Deck::new();
+  let mut deck = Deck::new();
+
+  println!("Heres your deck: {:#?}", deck);
 
   deck.shuffle();
 
+  let cards = deck.deal(3);
+
   println!("Heres your deck: {:#?}", deck);
+  println!("Heres your hand: {:#?}", cards);
 }
 
